dadeb9faba4e3a9e70323bbe35eb64c9d8ab7b99
Yann Dubois <yannd@fb.com>
2020-07-29 16:49:48 -0400

    [GIT]: remove built sire

--- jupyter/_build/jupyter_execute/Untitled.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-url = 'https://www.ishares.com/us/products/239705/ishares-phlx-semiconductor-etf/\
-1467271812596.ajax?fileType=csv&fileName=SOXX_holdings&dataType=fund'
-
-df = pd.read_csv(url, skiprows=9)  
-
-df
-
